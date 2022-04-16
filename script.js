dbd1910306ef9f106d5a046f07201be59c933850
SlavaChekhunov <slava97@my.yorku.ca>
2022-04-15 23:38:14 -0400

    added nultiple animations on h1 and skills section

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-const textElement = document.querySelector(".stretch");
 
-for (let i = 0; i < textElement.length; i++) {
-  textElement[i].addEventListener("animationend", function (e) {
-    textElement[i].classList.remove(".animated");
+const elements = document.getElementsByClassName('rubber-stretch');
+
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('animationend', function (e) {
+    elements[i].classList.remove('animated');
   });
 
-  textElement[i].addEventListener("mouseover", function (e) {
-    textElement[i].classList.add(".animated");
-  });
+  elements[i].addEventListener('mouseover', function (e) {
+    elements[i].classList.add('animated');
+  })
 }
 
-// const letters = text.split('');
+//tagCloud
+const Texts = [
+  "HTML5",
+  "CSS3",
+  "JAVASCRIPT",
+  "Git & Command Line",
+  "REACT",
+  "SASS",
+  "GitHub",
+  "Pair-Programming",
+  "Responsive Design",
+  "Firebase",
+  "Rest APIs",
+  "JQUERY",
+  "Version Control",
+];
 
-// let html = '';
+let tagCloud = new TagCloud('.Sphere', Texts, {
+  radius: 200,
+  maxSpeed: 'normal',
+  initSpeed: 'fast',
+  direction: 135,
+  keep: true,
+});
 
-// const makeSpan = letter => `<span class="rubber-letter">${letter}</span>`;
-
-// letters.forEach(letter => (html += makeSpan(letter)));
-
-// textElement.innerHTML = html;
+const color = 'green';
+document.querySelector('.Sphere').style.color = color;
+//tagCloud done
